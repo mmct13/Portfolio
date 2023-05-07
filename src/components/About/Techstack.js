1889da517c7396be545d70150de9c24f7dc45ba7
mmct13 <marshallchrist@yahoo.com>
2023-05-07 12:21:44 +0000

    Ajout des noms des outils et techs

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -22,25 +22,32 @@ function Techstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
+          <h5>C++</h5>
       </Col>
         <Col xs={4} md={2} className="tech-icons">
             <SiHtml5 />
+            <h5>HTML5</h5>
         </Col>
         <Col xs={4} md={2} className="tech-icons">
             <SiPhp />
+            <h5>PHP</h5>
         </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+          <h5>JavaScript</h5>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+          <h5>NodeJs</h5>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+          <h5>ReactJS</h5>
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+          <h5>Python</h5>
       </Col>
 
     </Row>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -16,25 +16,32 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiLinux />
+          <h5>Linux</h5>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+          <h5>VScode</h5>
       </Col>
         <Col xs={4} md={2} className="tech-icons">
             <SiPhpstorm />
+            <h5>PhpStorm</h5>
         </Col>
         <Col xs={4} md={2} className="tech-icons">
             <SiPostman />
+            <h5>Postman</h5>
         </Col>
         <Col xs={4} md={2} className="tech-icons">
             <SiPhpmyadmin />
+            <h5>PhpMyAdmin</h5>
         </Col>
         <Col xs={4} md={2} className="tech-icons">
             <SiMysql />
+            <h5>MySQL</h5>
         </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <SiHeroku />
+          <h5>Heroku</h5>
       </Col>
     </Row>
   );
